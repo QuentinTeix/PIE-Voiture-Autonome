1b1988def93bcf42c0e61c3af2dd01d46419d02e
Benjamin-coder1 <62752538+Benjamin-coder1@users.noreply.github.com>
2021-01-03 11:10:04 +0100

    Update testzonesafe.py

--- pie-voiture-autonome-master/Trajectoire/testzonesafe.py
+++ pie-voiture-autonome-master/Trajectoire/testzonesafe.py
@@ -1,27 +1,40 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-#MMMR=[(thetai,ri,xi,yi,xpi,ypi)]
-
-
 import copy
 from zonesafe import *
+
+"""
+Ce module permet de tester le bon fonctionement de la zone safe avant de tester le bon fonctionement de trouvecible
+MMMR=[(thetai,ri,xi,yi,xpi,ypi)]
+
+"""
+#Nombre d'angle de mesure du lidar
 N=720
-lidar=[]
+#rayon cercle circonscrit
 rv=20
+# marge 
 m=5
-i=0
+
+#on remplit liste valeurs lidar toute mesure situé a distance r donc on a arc de cercle
+lidar=[]
 r=50
 
+i=0
 while i<N:
     lidar.append((i,r))
     i+=1
 
+   
+#calcul zone safe 
 MMMR=zonesafe(lidar,rv,m)
+
+#on trace 
 i=0
 while i<len(MMMR):
+    #bord circuit
     plt.plot(MMMR[i][2], MMMR[i][3],"b:o")
+    #bord zone safe 
     plt.plot(MMMR[i][4], MMMR[i][5],"r:o")
     i=i+1
     
