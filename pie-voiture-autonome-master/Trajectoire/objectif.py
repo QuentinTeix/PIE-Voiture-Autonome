1d058ae2dda36cec4d792d2817ce94fd7bbaf097
QuentinTeix <62744094+QuentinTeix@users.noreply.github.com>
2021-05-04 14:53:34 +0200

    Delete pie-voiture-autonome-master directory

--- pie-voiture-autonome-master/Trajectoire/objectif.py
+++
@@ -1,9 +0,0 @@
-from math import *
-"""
-Distance pouvant être parcourue en fonction des coordonnées d'une cible, de la vitesse v et d'un temps deltat
-"""
-def objectif(cible,v,deltat):
-    K=atan(cible[1]/cible[0])
-    xo=v*deltat/sqrt(1+K**2)
-    yo=K*xo
-    return([xo,yo])
